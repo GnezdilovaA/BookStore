5bd8a19d9962c8d31760c66ab0592f1acd2a1c72
Ангелина Гнездилова <odnokletochniy.kit@yandex.ru>
2022-03-22 18:59:45 +0300

    Revert "Revert "Revert "</>"""
    This reverts commit fa63b36dea74d1f777a2535b306d1281976a6c7c.

--- BookStore.Domain/Concrete/EFBookRepository.cs
+++ BookStore.Domain/Concrete/EFBookRepository.cs
@@ -7,38 +7,10 @@ namespace BookStore.Domain.Concrete
     public class EFBookRepository : IBookRepository
     {
         EFDbContext context = new EFDbContext();
+
         public IEnumerable<Book> Books
         {
             get { return context.Books; }
-        }
-        public void SaveBook (Book book)
-        {
-            if (book.BookId == 0)
-                context.Books.Add(book);
-            else
-            {
-                Book dbEntry = context.Books.Find(book.BookId);
-                if (dbEntry != null)
-                {
-                    dbEntry.Name = book.Name;
-                    dbEntry.Description = book.Description;
-                    dbEntry.Quantity = book.Quantity;
-                    dbEntry.Author = book.Author;
-                    dbEntry.Price = book.Price;
-
-                }
-            }
-            context.SaveChanges();
-        }
-        public Book DeleteBook(int bookId)
-        {
-            Book dbEntry = context.Books.Find(bookId);
-            if (dbEntry !=null)
-            {
-                context.Books.Remove(dbEntry);
-                context.SaveChanges();
-            }
-            return dbEntry;
         }
     }
 }
