fa63b36dea74d1f777a2535b306d1281976a6c7c
Ангелина Гнездилова <odnokletochniy.kit@yandex.ru>
2022-03-22 18:52:40 +0300

    Revert "Revert "</>""
    This reverts commit d5f228a968685a5c660d9a12f2c8f9db9a2713b2.

--- BookStore.Domain/Abstract/IBookRepository.cs
+++ BookStore.Domain/Abstract/IBookRepository.cs
@@ -10,5 +10,7 @@ namespace BookStore.Domain.Abstract
      public interface IBookRepository
     {
         IEnumerable<Book> Books { get; }
+        void SaveBook(Book book);
+        Book DeleteBook(int bookId);
     }
 }
